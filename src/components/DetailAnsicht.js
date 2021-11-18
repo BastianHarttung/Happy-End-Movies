ca21533085bccfeee81a5571553298a636ea1d4a
BastianHarttung <info@harttung-fotografie.de>
2021-11-18 15:12:22 +0100

    vote for happy end and show smile

--- src/components/DetailAnsicht.js
+++ src/components/DetailAnsicht.js
@@ -1,12 +1,21 @@
 import classes from "./DetailAnsicht.module.css";
 import emptyImage from "../assets/img/movie-poster.png"
+import {useState} from "react";
+import {FaSmileBeam, FaSadTear} from "react-icons/all";
 
 const DetailAnsicht = (props) => {
+
     const imageUrl = "https://image.tmdb.org/t/p/w500" + props.movie.poster_path;
 
     const genres = props.movie.genres ? props.movie.genres : []
 
-    console.log(props.movie.genres)
+    const [movieForDb, setMovieForDb] = useState(props.movie)
+
+    const [happyMovie, setHappyMovie] = useState()
+
+    function saveInfosInVariable(movieHasHappyEnd) {
+        setMovieForDb({...props.movie, has_happy_end: movieHasHappyEnd})
+    }
 
     return (
         <section className={classes.detailsSection}>
@@ -14,20 +23,43 @@ const DetailAnsicht = (props) => {
                 <img className={classes.detailsImage} src={props.movie.poster_path ? imageUrl : emptyImage}
                      alt="Poster"/>
                 <div className={classes.detailsContainer}>
-                    <h3>{props.movie.title}</h3>
 
-                    <div className={classes.filmInfos}>
-                        <div>
-                            {genres.map((genre, index) => {
-                                return <div key={index}>{genre}</div>
-                            })}
+                    <div>
+                        <h3>{props.movie.title}</h3>
+
+                        <div className={classes.filmInfos}>
+                            <div>
+                                {genres.map((genre, index) => {
+                                    return <div key={index}>{genre}</div>
+                                })}
+                            </div>
+                            <div className={classes.voting}>{props.movie.vote_average}</div>
                         </div>
-                        <div className={classes.voting}>{props.movie.vote_average}</div>
+
+                        <div>Beschreibung: {props.movie.overview}</div>
+
                     </div>
 
+                    <div>
+                        <div>Happy End ?</div>
+                        <div className={classes.smileys}>
+                            <FaSmileBeam onClick={() => {
+                                setHappyMovie(true)
+                                saveInfosInVariable(true)
+                            }}
+                                         className={happyMovie ? classes.smileyLaugh : classes.smiley}></FaSmileBeam>
+                            <FaSadTear onClick={() => {
+                                setHappyMovie(false);
+                                saveInfosInVariable(false)
+                            }}
+                                       className={!happyMovie ? classes.smileySad : classes.smiley}></FaSadTear>
+                        </div>
+                    </div>
 
-                    <div>{props.movie.overview}</div>
-                    <button>Happy End ?</button>
+                    {/*<button onClick={() => {
+                        console.log(movieForDb)
+                    }}>log
+                    </button>*/}
 
                 </div>
             </div>
