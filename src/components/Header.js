060a795b41a60573621280af359fbd5a5be57e16
BastianHarttung <info@harttung-fotografie.de>
2022-02-12 14:51:30 +0100

    design(Header): adding logo to Header and change favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
 import Navigation from "./Navigation";
-import {FaFilm} from "react-icons/all";
-import classes from "./Header.module.css";
+import classes from "./Header.module.scss";
 import {Link} from "react-router-dom";
 import iconSignout from "../assets/icons/sign-out-alt_light.svg"
+import {ReactComponent as HappyLogo} from '../assets/logos/Happy-End_logo-mit-Text.svg';
 
 
 const Header = (props) => {
     return (
         <header className={classes.header}>
-            <Link to='/'>
-                <FaFilm style={{color: 'white', width: "50px", height: "50px"}}/>
+            <Link to='/'
+                  className={classes.headerLogoLink}>
+                <HappyLogo className={classes.happyLogo}/>
             </Link>
-            <Navigation></Navigation>
-            <Link to="/login"
-                  className={classes.logoutLink}>
-                <div className={classes.linkContainer}>
-                    <div>Logout</div>
-                    <img src={iconSignout}
-                         className={classes.icons}/>
-                </div>
-            </Link>
+
+            <Navigation/>
+
+            <div className={classes.userContainer}>
+                <div className={classes.username}>BASTIAN</div>
+                <Link to="/login"
+                      className={classes.logoutLink}>
+                    <div className={classes.linkContainer}>
+                        <div>Logout</div>
+                        <img src={iconSignout}
+                             className={classes.icons}/>
+                    </div>
+                </Link>
+            </div>
+
         </header>
     )
 }
